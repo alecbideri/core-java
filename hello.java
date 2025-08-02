5919b0073079fdcbc048a4031fb7b266ab2e82a4
Bideri Alec <101627722+alecbideri@users.noreply.github.com>
2025-08-02 23:39:54 +0200

    first commit

---
+++ hello.java
@@ -0,0 +1,11 @@
+class hello {
+  public static void main(String[] args) {
+
+    int age = 25;
+    double height = 5.9;
+    String name = "Alex";
+
+    System.out.println(String.format("Name: %s, Age: %d, Height: %.1f", name, age, height));
+
+  }
+}
